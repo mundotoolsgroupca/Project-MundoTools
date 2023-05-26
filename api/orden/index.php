21bdb00cdb34247d65c8fd6bd4a8208b5af9bd4a
Glicht <rosalesvallorani@gmail.com>
2023-05-26 19:00:36 -0400

    cambios

--- api/orden/index.php
+++ api/orden/index.php
@@ -731,7 +731,7 @@ function agregar_orden($_DataPOST, $carritostorage, $check_correo, $correo)
     }
     if ($check_correo == true) {
         //$check_correo = $check_correo;
-
+        echo $correo;
         if (!validar_correo($correo)) {
             http_response_code(409); //codigo de conflicto
             $resultado = new stdClass();
