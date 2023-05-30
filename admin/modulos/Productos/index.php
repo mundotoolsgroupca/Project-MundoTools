100f7a2062957045f0858d351e71aa31e9344a99
Glicht <rosalesvallorani@gmail.com>
2023-05-30 09:25:41 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -4,7 +4,7 @@
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
             <p class="[ text-lg font-bold ]">Nuevo Producto</p>
             <span id="NuevoProductoLoader"></span>
-            <span class='[ block ] [ md:hidden ] [ lg:hidden ]'>
+            <span onclick="toggle('nuevo_producto_modulo')" class='[ block ] [ md:hidden ] [ lg:hidden ]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
@@ -87,7 +87,7 @@
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
             <p class="[ text-lg font-bold ]">Productos</p>
             <span id="ProductosLoader"></span>
-            <span class='[ block ] [ md:hidden ] [ lg:hidden ]'>
+            <span onclick="toggle('producto_modulo')" class='[ block ] [ md:hidden ] [ lg:hidden ]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
@@ -231,10 +231,10 @@
             toggle(this);
         });
 
-        function toggle(div) {
-            $("#botonToggle").click(function() {
-                $(`#${div.id}`).toggle();
-            });
+        function toggle(id) {
+
+            $(`#${id}`).toggle();
+
         }
         tabla_producto = new DataTable('#tabla_producto');
         Producto_consulta();
