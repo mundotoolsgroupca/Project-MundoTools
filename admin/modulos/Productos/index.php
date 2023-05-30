9a89fc6c9b43c7f29c9b773cfc9437ddbac9f770
Glicht <rosalesvallorani@gmail.com>
2023-05-30 09:37:36 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -409,7 +409,17 @@
                             },
                             "data": response.data,
                             "columns": [{
-                                    "data": "id_grupo"
+                                    "data": null,
+                                    "bSortable": false,
+                                    "mRender": function(data, type, value) {
+                                        return `<button
+                                                type="button"
+                                                data-te-ripple-init
+                                                data-te-ripple-color="light"
+                                                class="rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:bg-neutral-100 hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700 dark:hover:bg-neutral-700">
+                                                ${data.id_grupo}
+                                                </button>`;
+                                    }
                                 }, {
                                     "data": "nombre"
                                 },
