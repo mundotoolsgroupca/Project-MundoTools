34b45c02c48cb2763adcf0f45e53c3496f450e63
Glicht <rosalesvallorani@gmail.com>
2023-05-30 12:20:35 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -605,6 +605,9 @@
             headers: {
                 'x-csrf-token': $('meta[name="csrf-token"]').attr('content')
             },
+            data: {
+                id_agrupado: id
+            },
             beforeSend: () => {
                 $('#ProductosLoader').html(`<div class="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
                                     <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
