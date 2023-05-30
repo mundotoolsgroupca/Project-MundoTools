ab389d116e6b8bcc30d2eb0c6059ea12a68804bf
Glicht <rosalesvallorani@gmail.com>
2023-05-30 11:52:45 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -233,7 +233,7 @@
     <div class="[ h-auto rounded-lg bg-white p-6 shadow-lg w-full ] [   lg:h-full  ]">
 
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
-            <span onclick='toggle_agrupados() '>
+            <span class="cursor-pointer text-[#FBAA35]" onclick="toggle_agrupados(),toggle('producto_modulo'),toggle('nuevo_producto_modulo')">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                 </svg>
