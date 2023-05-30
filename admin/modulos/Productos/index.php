14b7ee9e2c644bf0d312171f29d361275e63a9d3
Glicht <rosalesvallorani@gmail.com>
2023-05-30 09:23:57 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -4,6 +4,12 @@
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
             <p class="[ text-lg font-bold ]">Nuevo Producto</p>
             <span id="NuevoProductoLoader"></span>
+            <span class='[ block ] [ md:hidden ] [ lg:hidden ]'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+
+            </span>
         </div>
         <div id="nuevo_producto_modulo">
             <form id="nuevoproductoform">
@@ -81,6 +87,12 @@
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
             <p class="[ text-lg font-bold ]">Productos</p>
             <span id="ProductosLoader"></span>
+            <span class='[ block ] [ md:hidden ] [ lg:hidden ]'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+
+            </span>
         </div>
         <div id="producto_modulo">
             <div class="mb-3 w-full">
