ba7db164574ff66a248a27a9dbfa7b764e2c535a
Glicht <rosalesvallorani@gmail.com>
2023-05-30 10:44:23 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -235,7 +235,7 @@
         <div class="[ flex items-center gap-1 flex-nowrap mb-3 ]">
             <p id="titulo_tabla_agrupados" class="[ text-lg font-bold ]">LOREM</p>
             <span id="ProductosLoader"></span>
-            <span onclick="toggle('producto_modulo')" class='[ block ] [ md:hidden ] [ lg:hidden ]'>
+            <span onclick="toggle_agrupados()" class='[ block ] [ md:hidden ] [ lg:hidden ]'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
@@ -266,6 +266,11 @@
             $("#tabla_productos_agrupados").attr("view", '1'); //actualizamos el estado
         }
 
+    }
+
+    function toggle_agrupados() {
+        let status_view = $("#tabla_productos_agrupados").attr("view"); //obtenemos la propiedad personalizada para saber si el modulo esta a la vista
+        $(`#tabla_productos_agrupados`).slideToggle("slow");
     }
 
     tabla_producto = new DataTable('#tabla_producto');
