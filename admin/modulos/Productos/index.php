53d1aa20ce8ef50bbe9ca21063ee959c2a1b5df8
Glicht <rosalesvallorani@gmail.com>
2023-05-31 14:56:43 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -23,6 +23,28 @@
                 </div>
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
+                    <input maxlength="11" data-te-input-showcounter="true" max name="id_grupo" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="id_grupo" value="<?php
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                include "../../php/conexion.php";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                $consulta = ' SELECT id_grupo FROM productos_agrupados ORDER BY id_grupo DESC LIMIT 1;';
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                $resultado = mysqli_query($conexion, $consulta);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                if ($resultado) {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    $data = [];
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    $row = mysqli_fetch_assoc($resultado);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    echo $row;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                } else {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    echo "0";
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ?>" placeholder="id" required />
+                    <label for="id_grupo" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  ">Id del Prducto
+                    </label>
+                    <div class="absolute w-full text-sm text-neutral-500 dark:text-neutral-200" data-te-input-helper-ref></div>
+                </div>
+
+                <div class="relative mb-6" data-te-input-wrapper-init>
                     <input maxlength="40" data-te-input-showcounter="true" name="nombreproducto" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleInput7" placeholder="Name" required />
                     <label for="exampleInput7" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  ">Nombre del Producto
                     </label>
@@ -378,7 +400,7 @@
 
 
     $('#tabla_agrupados_producto tbody').on('click', 'tr', function() {
-        
+
 
         data = tabla_agrupados_data.row(this).data();
         $('#ModalEditar_agrupadosID').val(data.id_grupo);
@@ -862,7 +884,7 @@
 
 
     function editar_producto_agrupado() {
-        
+
         $.ajax({
             url: "./api-v1/productos/agrupados.php",
             type: 'POST',
