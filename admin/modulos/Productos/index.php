e3fc3c211b7bc5242b937c3b9704ea48299e1b17
Glicht <rosalesvallorani@gmail.com>
2023-05-31 09:55:32 -0400

    cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -380,7 +380,7 @@
 
         data = tabla_agrupados_data.row(this).data();
         $('#ModalEditar_agrupadosID').val(data.id);
-        $('#ModalEditar_agrupadosTitulo').val(data.nombre);
+        $('#ModalEditar_agrupadosTitulo').html(data.nombre);
         $('#ModalEditar_agrupadosdescripcionProducto').val(data.caracteristica);
         $('#ModalEditar_agrupadosdescripcionProducto2').val(data.caracteristica2);
         $('#ModalEditar_agrupadosdescripcionProducto3').val(data.caracteristica3);
