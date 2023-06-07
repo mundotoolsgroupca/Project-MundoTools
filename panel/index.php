cc875f7a7edc3b5810a74afe0dc909cbc0a33734
Glicht <rosalesvallorani@gmail.com>
2023-06-07 15:53:44 -0400

    Cambios

--- panel/index.php
+++ panel/index.php
@@ -181,6 +181,16 @@ if (!isset($_SESSION['usuario'])) {
                                 </a>
                             </li>
                             <li>
+                                <a href="../tienda" data-te-ripple-init data-te-ripple-color="light" class="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                    </svg>
+
+
+                                    <span>Volver</span>
+                                </a>
+                            </li>
+                            <li>
                                 <a href="../php/logout.php" data-te-ripple-init data-te-ripple-color="light" class="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
