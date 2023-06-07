44294b758644930baac06f9b48fe7363ff4102cb
Glicht <rosalesvallorani@gmail.com>
2023-06-07 09:06:41 -0400

    Cambios

--- tienda.php
+++ tienda.php
@@ -192,7 +192,7 @@ session_start();
     </nav>
 
 
-
+<!----
     <div id="carouselExampleIndicators" class="relative mt-3" data-te-carousel-init data-te-carousel-slide>
         <div class="absolute right-0 bottom-0 left-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0" data-te-carousel-indicators>
             <button type="button" data-te-target="#carouselExampleIndicators" data-te-slide-to="0" data-te-carousel-active class="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none" aria-current="true" aria-label="Slide 1"></button>
@@ -237,7 +237,7 @@ session_start();
             <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Next</span>
         </button>
     </div>
-
+----->
 
     <div class="[ relative ]">
 
