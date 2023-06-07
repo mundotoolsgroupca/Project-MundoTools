f5ff336866aa9a90bb25be0b5a68b5c282c9b522
Glicht <rosalesvallorani@gmail.com>
2023-06-07 15:54:18 -0400

    Cambios

--- panel/index.php
+++ panel/index.php
@@ -181,7 +181,7 @@ if (!isset($_SESSION['usuario'])) {
                                 </a>
                             </li>
                             <li>
-                                <a href="../tienda" data-te-ripple-init data-te-ripple-color="light" class="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
+                                <a href="../tienda.php" data-te-ripple-init data-te-ripple-color="light" class="hover:bg-gray-500 hover:bg-opacity-10 hover:text-blue-600 flex items-center text-gray-700 py-1.5 px-4 rounded space-x-2 cursor-pointer">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                                     </svg>
