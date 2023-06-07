54f66ffb88b89c493b31203a6ff457c710234d55
Glicht <rosalesvallorani@gmail.com>
2023-06-07 16:18:02 -0400

    Cambios

--- panel/controladores/modulos/tokens.php
+++ panel/controladores/modulos/tokens.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['usuario'])) {
                 </div>
 
                 <div class="relative mb-3" data-te-input-wrapper-init>
-                    <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleFormControlInputCounter" placeholder="Nombre del Responsable" data-te-input-showcounter="true" maxlength="20" />
+                    <input type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="nombre_del_responsable" placeholder="Nombre del Responsable" data-te-input-showcounter="true" maxlength="20" />
                     <label for="exampleFormControlInputCounter" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Nombre del Responsable
                     </label>
                     <div class="absolute w-full text-sm text-neutral-500 peer-focus:text-primary dark:text-neutral-200 dark:peer-focus:text-primary" data-te-input-helper-ref></div>
@@ -265,6 +265,8 @@ if (!isset($_SESSION['usuario'])) {
         }
 
         function tokens_generar() {
+
+            let nombre_del_responsable = $('#nombre_del_responsable').val();
             $.ajax({
                 // la URL para la petición
                 url: "../api/token/index.php",
@@ -275,7 +277,9 @@ if (!isset($_SESSION['usuario'])) {
                     'x-csrf-token': $('meta[name="csrf-token"]').attr('content')
                 },
 
-
+                data: {
+                    nombre_del_responsable: nombre_del_responsable
+                },
                 // especifica si será una petición POST o GET
                 type: 'POST',
 
