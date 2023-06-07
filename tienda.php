3e585f7f4e6213723077528c088785f9d8bbe8b4
Glicht <rosalesvallorani@gmail.com>
2023-06-07 11:02:23 -0400

    Cambios

--- tienda.php
+++ tienda.php
@@ -152,7 +152,7 @@ session_start();
                                 <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30" href="#" data-te-dropdown-item-ref>Opciones</a>
                             </li>
                             <li>
-                                <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30" href="./php/logout.php" data-te-dropdown-item-ref>Cerrar Sesion</a>
+                                <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-white/30" href="../php/logout.php" data-te-dropdown-item-ref>Cerrar Sesion</a>
                             </li>
                         </ul>
                     </div>
