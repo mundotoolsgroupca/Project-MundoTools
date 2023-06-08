39b7477f28ea0068830a2ae600d534c8cc91f782
Glicht <rosalesvallorani@gmail.com>
2023-06-08 10:27:17 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -276,7 +276,7 @@
 
 
     <!-- Modal -->
-    <div data-te-modal-init class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="agregar_producto" data-te-backdrop="static" data-te-keyboard="false" tabindex="-1" aria-labelledby="agregar_productoLabel" aria-hidden="true">
+    <div data-te-modal-init class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="modal_agregar_producto" data-te-backdrop="static" data-te-keyboard="false" tabindex="-1" aria-labelledby="modal_agregar_productoLabel" aria-hidden="true">
         <div data-te-modal-dialog-ref class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
             <div class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
                 <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
@@ -437,6 +437,10 @@
 
     modal_editar_agrupados = document.getElementById("modal_editar_agrupados");
     modal_editar_agrupados = new te.Modal(modal_editar_agrupados);
+
+
+    modal_agregar_producto = document.getElementById("modal_agregar_producto");
+    modal_agregar_producto = new te.Modal(modal_agregar_producto);
 
     form = document.getElementById('nuevogrupoform');
 
