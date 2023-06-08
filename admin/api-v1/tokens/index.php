ba4e692261a00dc60c3954930f3a8081e18854c9
Glicht <rosalesvallorani@gmail.com>
2023-06-08 08:26:44 -0400

    Cambios

--- admin/api-v1/tokens/index.php
+++ admin/api-v1/tokens/index.php
@@ -48,7 +48,7 @@ switch ($method) {
                 vendedores_tokens.fecha_creacion,
                 vendedores_tokens.fecha_vencimiento,
                 vendedores_tokens.token,
-                vendedores_tokens.`status`
+                vendedores_tokens.`status`,
                 vendedores_tokens.`responsable`
                 FROM
                 vendedores_tokens
