79eb993fe3c9873db54d749d7542cc9ac3aedf73
Glicht <rosalesvallorani@gmail.com>
2023-06-08 10:39:26 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -302,7 +302,11 @@
                             <input class="[ hidden ]" type="text" name="ModalAgregar_agrupadosID" id="ModalAgregar_agrupadosID" />
                             <input class="[ hidden ]" type="text" name="ModalEditar_id_producto" id="ModalEditar_id_producto" />
 
-
+                            <div class="relative w-full" data-te-input-wrapper-init>
+                                <input maxlength="40" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="ModalAgregar_agrupadosidproducto" id="ModalAgregar_agrupadosidproducto" placeholder="Example label" />
+                                <label for="ModalAgregar_agrupadosidproducto" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200">Id Producto
+                                </label>
+                            </div>
 
                             <div class="relative w-full" data-te-input-wrapper-init>
                                 <input maxlength="40" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="ModalAgregar_agrupadoscaracteristicaProducto" id="ModalAgregar_agrupadoscaracteristicaProducto" placeholder="Example label" />
