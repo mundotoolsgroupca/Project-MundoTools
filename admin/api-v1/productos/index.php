6cc458a52fd1868ecdfa5c6321b6172f04bbc371
Glicht <rosalesvallorani@gmail.com>
2023-06-12 18:04:12 -0400

    CAmbios

--- admin/api-v1/productos/index.php
+++ admin/api-v1/productos/index.php
@@ -541,10 +541,6 @@ switch ($method) {
                             $consulta = "CALL adm_editar_producto( '$id_grupo', '$id', '0', '0', '0','0','0','0','0','0','$stock','" . $_SESSION['Usuario']['id'] . "','2')"; //editar el stock
                         }
 
-                        echo  $consulta;
-
-                        return;
-
 
 
                         $resultado = mysqli_query($conexion, $consulta);
