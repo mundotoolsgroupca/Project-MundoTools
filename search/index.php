999ae4301fe8ee43d90492f510d2891561aec5b1
Glicht <rosalesvallorani@gmail.com>
2023-06-13 07:57:52 -0400

    Cambios

--- search/index.php
+++ search/index.php
@@ -450,7 +450,7 @@ session_start();
                                     </div>
 
                                     <div>
-                                        <div class="flex flex-col">
+                                        <div class="flex flex-col h-72">
                                             <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                                                 <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                                                     <div class="overflow-hidden">
