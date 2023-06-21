2071e2f7a0249121a1b27991ffd10dc10216d1f2
Glicht <rosalesvallorani@gmail.com>
2023-06-21 15:15:29 -0400

    Cambios

--- admin/api-v1/ordenes/index.php
+++ admin/api-v1/ordenes/index.php
@@ -134,11 +134,11 @@ switch ($method) {
 
                     include_once '../../php/conexion.php';
 
-                    echo $_SESSION['Usuario']['nombre'];
-                    return;
+                    $nombre_admin =  $_SESSION['Usuario']['nombre'];
+
 
                     $id_vendedor = $_SESSION['usuario']['id'];
-                    $consulta = "CALL  adm_cancelar_orden('$id_vendedor','$id_orden','El Admin " . $_SESSION['Usuario']['nombre'] . " Cancelo la Orden Nro $id_orden');";
+                    $consulta = "CALL  adm_cancelar_orden($id_vendedor,$id_orden,'El Admin $nombre_admin Cancelo la Orden Nro $id_orden');";
 
                     $resultado = mysqli_query($conexion, $consulta);
                     $newid = "";
