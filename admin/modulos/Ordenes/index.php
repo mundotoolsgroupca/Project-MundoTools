db54f11f088628214866436e47174e86c74cfcfe
Glicht <rosalesvallorani@gmail.com>
2023-06-26 09:20:07 -0400

    Cambios

--- admin/modulos/Ordenes/index.php
+++ admin/modulos/Ordenes/index.php
@@ -49,54 +49,37 @@
 
     </div>
 
-    <div data-te-modal-init class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none" id="modalordenes" tabindex="-1" aria-labelledby="modalordenes" aria-modal="true" role="dialog">
-        <div data-te-modal-dialog-ref class="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
-            <div class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-                <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                    <!--Modal title-->
-                    <h5 class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200" id="modalordenesLabel">
-                        Modal title
-                    </h5>
-                    <!--Close button-->
-                    <button type="button" class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none" data-te-modal-dismiss aria-label="Close">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-
-                <!--Modal body-->
-                <div class="relative p-4">
-                    <div>
-                        <table>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quis quisquam nam adipisci? Iusto, tempore qui dolor quo alias officiis, architecto est autem et culpa perferendis sed at explicabo provident.
-                        </table>
-                    </div>
-
-                </div>
-
-                <!--Modal footer-->
-                <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                    <button type="button" class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
-                        Close
-                    </button>
-                    <button type="button" class="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" data-te-ripple-init data-te-ripple-color="light">
-                        Save changes
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <script>
-        modal_ordenes = new te.Modal(document.getElementById("modalordenes"));
         tablaordenes = new DataTable('#Ordenestabla');
         ordenes();
 
         function pedido_det(id) {
 
-            modal_ordenes.show();
-
+            Swal.fire({
+                width: 600,
+                html: `<div class="flex flex-col overflow-x-auto">
+                        <div class="sm:-mx-6 lg:-mx-8">
+                            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                            <div class="overflow-x-auto">
+                                <table class="min-w-full text-left text-sm font-light">
+                                <thead class="border-b font-medium dark:border-neutral-500">
+                                    <tr>
+                                    <th scope="col" class="px-6 py-4 whitespace-nowrap">Id</th>
+                                    <th scope="col" class="px-6 py-4 whitespace-nowrap">Producto</th>
+                                    <th scope="col" class="px-6 py-4 whitespace-nowrap">Cantidad</th>
+                                    <th scope="col" class="px-6 py-4 whitespace-nowrap">Precio Unitario</th>
+                                    <th scope="col" class="px-6 py-4 whitespace-nowrap">Total</th> 
+                                    </tr>
+                                </thead>
+                                <tbody>           
+                                </tbody>
+                                </table>
+                            </div>
+                            </div>
+                        </div>
+                        </div>`
+            });
             $.ajax({ //se manda los valores obtenido a php
                 url: "./api-v1/ordenes/orden_det.php",
                 type: 'POST',
