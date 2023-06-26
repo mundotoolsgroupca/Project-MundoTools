96c42aef1974bccddf3401b2431301eca5b20a62
Glicht <rosalesvallorani@gmail.com>
2023-06-26 10:24:23 -0400

    CAmbios

--- admin/modulos/Ordenes/index.php
+++ admin/modulos/Ordenes/index.php
@@ -52,6 +52,7 @@
 
     <script>
         tablaordenes = new DataTable('#Ordenestabla');
+        tabla_det_temp = new DataTable('#modal_table_temp');
         ordenes();
 
         async function pedido_det(id) {
@@ -143,6 +144,14 @@
                             </div>
                             </div>
                         </div>
+
+                        <button
+  type="button"
+  data-te-ripple-init
+  data-te-ripple-color="light"
+  class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+  Click me
+</button>
                         </div>`
             });
             if (data_srv.result) {
