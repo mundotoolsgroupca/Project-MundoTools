f7408db7644e020254212457175ecb8bea8c42b6
Glicht <rosalesvallorani@gmail.com>
2023-06-26 12:34:02 -0400

    Cambios

--- admin/modulos/Ordenes/index.php
+++ admin/modulos/Ordenes/index.php
@@ -221,6 +221,14 @@
 
         });
 
+        function validarNumero(event) {
+            var charCode = event.which ? event.which : event.keyCode;
+            if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+                return false;
+            }
+            return true;
+        }
+
         async function savePDF(id) {
 
 
