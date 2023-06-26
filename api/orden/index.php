2858cfcf244f6ed1101d7411b5751c32ca3df392
Glicht <rosalesvallorani@gmail.com>
2023-06-26 08:44:41 -0400

    CAmbios

--- api/orden/index.php
+++ api/orden/index.php
@@ -552,7 +552,7 @@ function agregardetalle($id, $data, $carritostorage, $check_correo, $correo)
         $simbolo = $carritostorage[$i]['simbolo'];
         //$consulta2 = "CALL usr_agregar_orden_det($id,'$id_producto', $cantidad, $precio)";
 
-       
+     
         $resultado2 = mysqli_query($conexion, $consulta2);
 
         if ($resultado2) {
