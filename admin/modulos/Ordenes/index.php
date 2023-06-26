38899414b350ad467b6c21e8afc429a383bf2ee5
Glicht <rosalesvallorani@gmail.com>
2023-06-26 11:38:56 -0400

    CAmbios

--- admin/modulos/Ordenes/index.php
+++ admin/modulos/Ordenes/index.php
@@ -52,7 +52,7 @@
 
     <script>
         tablaordenes = new DataTable('#Ordenestabla');
-        tabla_det_temp = new DataTable('#modal_table_temp');
+
         ordenes();
 
         async function pedido_det(id) {
@@ -134,8 +134,7 @@
                                         <th scope="col" class="px-6 py-4 whitespace-nowrap">Id</th>
                                         <th scope="col" class="px-6 py-4 whitespace-nowrap">Producto</th>
                                         <th scope="col" class="px-6 py-4 whitespace-nowrap">Cantidad</th>
-                                        <th scope="col" class="px-6 py-4 whitespace-nowrap">Precio Unitario</th>
-                                        <th scope="col" class="px-6 py-4 whitespace-nowrap">Total</th> 
+                                        <th scope="col" class="px-6 py-4 whitespace-nowrap">Precio Unitario</th> 
                                     </tr>
                                 </thead>
                                 <tbody>           
@@ -145,15 +144,11 @@
                             </div>
                         </div>
 
-                        <button
-  type="button"
-  data-te-ripple-init
-  data-te-ripple-color="light"
-  class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-  Click me
-</button>
+
                         </div>`
             });
+
+            tabla_det_temp = new DataTable('#modal_table_temp');
             if (data_srv.result) {
 
 
@@ -173,8 +168,7 @@
                         <div class="relative mb-3" data-te-input-wrapper-init>
                         <input class="border rounded-lg px-3 border-black" type="text"  value='${item.cantidad}'>
                         </div></td>
-                        <td class="whitespace-nowrap px-6 py-4">${item.precio}</td>
-                        <td class="whitespace-nowrap px-6 py-4">${(item.cantidad * item.precio) +" "+ item.simbolo }</td> 
+                        <td class="whitespace-nowrap px-6 py-4">${item.precio}</td> 
                     </tr>
                     `);
                 });
