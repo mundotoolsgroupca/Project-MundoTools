53cb9c26af9366b17b675b94bb6c1a1422c677d6
Glicht <rosalesvallorani@gmail.com>
2023-06-26 09:05:19 -0400

    CAmbios

--- admin/modulos/Ordenes/index.php
+++ admin/modulos/Ordenes/index.php
@@ -67,57 +67,38 @@
 
                 <!--Modal body-->
                 <div class="relative p-4">
-                    <div class="flex flex-col overflow-x-auto">
-                        <div class="sm:-mx-6 lg:-mx-8">
+                    <div class="flex flex-col">
+                        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                                <div class="overflow-x-auto">
-                                    <table class="min-w-full text-left text-sm font-light">
-                                        <thead class="border-b font-medium dark:border-neutral-500">
+                                <div class="overflow-hidden">
+                                    <table class="min-w-full text-center text-sm font-light">
+                                        <thead class="border-b bg-neutral-800 font-medium text-white dark:border-neutral-500 dark:bg-neutral-900">
                                             <tr>
-                                                <th scope="col" class="px-6 py-4">#</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
-                                                <th scope="col" class="px-6 py-4">Heading</th>
+                                                <th scope="col" class=" px-6 py-4">#</th>
+                                                <th scope="col" class=" px-6 py-4">First</th>
+                                                <th scope="col" class=" px-6 py-4">Last</th>
+                                                <th scope="col" class=" px-6 py-4">Handle</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <tr class="border-b dark:border-neutral-500">
-                                                <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
+                                                <td class="whitespace-nowrap  px-6 py-4 font-medium">1</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">Mark</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">Otto</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">@mdo</td>
                                             </tr>
                                             <tr class="border-b dark:border-neutral-500">
-                                                <td class="whitespace-nowrap px-6 py-4 font-medium ">2</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4 ">Cell</td>
+                                                <td class="whitespace-nowrap  px-6 py-4 font-medium">2</td>
+                                                <td class="whitespace-nowrap  px-6 py-4 ">Jacob</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">Thornton</td>
+                                                <td class="whitespace-nowrap  px-6 py-4">@fat</td>
                                             </tr>
-                                            <tr class="border-b ">
-                                                <td class="whitespace-nowrap px-6 py-4 font-medium ">3</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
-                                                <td class="whitespace-nowrap px-6 py-4">Cell</td>
+                                            <tr class="border-b dark:border-neutral-500">
+                                                <td class="whitespace-nowrap  px-6 py-4 font-medium">3</td>
+                                                <td colspan="2" class="whitespace-nowrap  px-6 py-4">
+                                                    Larry the Bird
+                                                </td>
+                                                <td class="whitespace-nowrap  px-6 py-4">@twitter</td>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -125,6 +106,7 @@
                             </div>
                         </div>
                     </div>
+
                 </div>
 
                 <!--Modal footer-->
