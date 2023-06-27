8ff281d7d8d43db00c423ced5a12320a0e3b49ba
Glicht <rosalesvallorani@gmail.com>
2023-06-27 08:42:07 -0400

    CAmbios

--- admin/api-v1/ordenes/aceptar_orden.php
+++ admin/api-v1/ordenes/aceptar_orden.php
@@ -71,8 +71,6 @@ switch ($method) {
                             $data = mysqli_fetch_assoc($resultado);
 
 
-                            echo "Cantidad consulta: " . $data['cantidad'];
-                            echo "Cantidad array: " . $cantidad;
 
                             // Check if there is enough stock for the requested quantity
                             if ($data['cantidad'] <= $cantidad) {
