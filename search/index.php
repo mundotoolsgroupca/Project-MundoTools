1c707beff0f40028e34ac7ac5a95f2332242f3a9
Glicht <rosalesvallorani@gmail.com>
2023-07-03 16:53:27 -0400

    Cambios

--- search/index.php
+++ search/index.php
@@ -366,8 +366,7 @@ session_start();
                         
                             ";
 
-                        echo $consulta2;
-                        return;
+                
 
                         $resultado2 = mysqli_query($conexion, $consulta2);
                         $totales = [];
