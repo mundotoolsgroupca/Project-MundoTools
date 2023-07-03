affc120ee380b96f4b4ea35c73f748374ffa0161
Glicht <rosalesvallorani@gmail.com>
2023-07-03 10:55:09 -0400

    Cambios

--- search/index.php
+++ search/index.php
@@ -594,7 +594,7 @@ session_start();
                                 }
 
 
-                        
+                                echo $consulta;
                                 $data = []; //variable que almacenara los resultados de la consulta
                                 $data['result'] = []; //cantida de paginas que tiene la consulta
                                 $data['num_pages'] = 0; //cantida de paginas que tiene la consulta
