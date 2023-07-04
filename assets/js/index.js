5ec05730f129407090d124e07af3ccdfd17faef9
Glicht <rosalesvallorani@gmail.com>
2023-07-04 16:10:08 -0400

    Cambios

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,8 +28,9 @@ const Carrito = {
   idcantidad: "Cantidad_produtos",
   idTotales: "CarritoTotales",
   add: (id, Count, Productosarr) => {
+    debugger
     /*Validamos el Stock con la cantidad que Se Requiere */
-    if (!Count > Productosarr.stock) {
+    if (!Count < Productosarr.stock) {
       //si la cantidad solicitada es mayor a la que queremos
       const Toast = Swal.mixin({
         toast: true,
