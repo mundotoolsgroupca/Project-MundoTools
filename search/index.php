1d2e67154c4134faf8490633f990052be369a7b2
Glicht <rosalesvallorani@gmail.com>
2023-07-04 14:15:42 -0400

    Cambios

--- search/index.php
+++ search/index.php
@@ -527,14 +527,20 @@ if (isset($_SESSION['token'])) {
 
                                     <div>
                                         <div class="flex flex-col h-40">
-                                            <table id="modal_tabla" class="min-w-full text-left text-sm font-light capitalize ">
-                                                <thead class="text-white">
+                                            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                                <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                                                    <div class="overflow-hidden">
+                                                        <table id="modal_tabla" class="min-w-full text-left text-sm font-light capitalize ">
+                                                            <thead class="text-white">
 
-                                                </thead>
-                                                <tbody>
+                                                            </thead>
+                                                            <tbody>
 
-                                                </tbody>
-                                            </table>
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
 
