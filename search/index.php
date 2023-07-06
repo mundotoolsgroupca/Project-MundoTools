d6b91d1da39d9f653a945927af8c5dc57b34aa79
Glicht <rosalesvallorani@gmail.com>
2023-07-06 08:58:25 -0400

    Cambios

--- search/index.php
+++ search/index.php
@@ -540,7 +540,7 @@ if (isset($_SESSION['token'])) {
 
                                     <!--Modal footer-->
                                     <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-                                        <button type="button" class="inline-block rounded bg-[#FBAA35] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
+                                        <button type="button" class="inline-block rounded bg-[#FBAA35] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" data-te-modal-dismiss data-te-ripple-init data-te-ripple-color="light">
                                             Cerrar
                                         </button>
 
