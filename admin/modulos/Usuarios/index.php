a664bf64f5081527e9cb1617e455d8c06389dfff
Glicht <rosalesvallorani@gmail.com>
2023-07-07 13:54:00 -0400

    Cambios

--- admin/modulos/Usuarios/index.php
+++ admin/modulos/Usuarios/index.php
@@ -22,6 +22,7 @@
                     <label for="apellido" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  ">Apellido
                     </label>
                     <div class="absolute w-full text-sm text-neutral-500 dark:text-neutral-200" data-te-input-helper-ref></div>
+
                 </div>
             </div>
             <div class="relative z-0 w-full mb-6 group" data-te-input-wrapper-init>
