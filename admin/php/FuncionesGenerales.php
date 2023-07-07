2c339b88c6ca6a1edc49a603512d2045102098b7
Glicht <rosalesvallorani@gmail.com>
2023-07-07 17:19:58 -0400

    CAmbios

--- admin/php/FuncionesGenerales.php
+++ admin/php/FuncionesGenerales.php
@@ -296,10 +296,11 @@ function eliminar_palabras_sql($cadena)
 
 function validar_string($string, $permitidos)
 {
+    /*
     $string = trim($string);
     if (strlen($string) == 0) {
         return false; // La cadena solo contiene espacios en blanco
-    }
+    }*/
     for ($i = 0; $i < strlen($string); $i++) {
         if (strpos($permitidos, $string[$i]) === false) {
             return false;
