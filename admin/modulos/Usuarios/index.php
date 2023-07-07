fff5f83f52c0c2c9a638a74bd8b273d2fae8189a
Glicht <rosalesvallorani@gmail.com>
2023-07-07 14:21:42 -0400

    CAmbios

--- admin/modulos/Usuarios/index.php
+++ admin/modulos/Usuarios/index.php
@@ -32,7 +32,7 @@
                 <div class="absolute w-full text-sm text-neutral-500 dark:text-neutral-200" data-te-input-helper-ref></div>
             </div>
             <div class="relative z-0 w-full mb-6 group" data-te-input-wrapper-init>
-                <input maxlength="30" data-te-input-showcounter="true" name="clave" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="clave" placeholder="Name" required />
+                <input maxlength="30" data-te-input-showcounter="true" name="clave" type="password" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none  [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="clave" placeholder="Name" required />
                 <label for="clave" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none  ">Clave
                 </label>
                 <div class="absolute w-full text-sm text-neutral-500 dark:text-neutral-200" data-te-input-helper-ref></div>
