1fb83fdb4bb830552bd9f7837e58af789865c843
Glicht <rosalesvallorani@gmail.com>
2023-07-07 16:27:11 -0400

    CAmbios

--- admin/modulos/Usuarios/index.php
+++ admin/modulos/Usuarios/index.php
@@ -53,32 +53,12 @@
                             <table class="min-w-full text-center text-sm font-light">
                                 <thead class="border-b bg-neutral-800 font-medium text-white dark:border-neutral-500 dark:bg-neutral-900">
                                     <tr>
-                                        <th scope="col" class=" px-6 py-4">#</th>
-                                        <th scope="col" class=" px-6 py-4">First</th>
-                                        <th scope="col" class=" px-6 py-4">Last</th>
-                                        <th scope="col" class=" px-6 py-4">Handle</th>
+                                        <th scope="col" class=" px-6 py-4">Nombre de Usuario</th>
+                                        <th scope="col" class=" px-6 py-4">status</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="border-b dark:border-neutral-500">
-                                        <td class="whitespace-nowrap  px-6 py-4 font-medium">1</td>
-                                        <td class="whitespace-nowrap  px-6 py-4">Mark</td>
-                                        <td class="whitespace-nowrap  px-6 py-4">Otto</td>
-                                        <td class="whitespace-nowrap  px-6 py-4">@mdo</td>
-                                    </tr>
-                                    <tr class="border-b dark:border-neutral-500">
-                                        <td class="whitespace-nowrap  px-6 py-4 font-medium">2</td>
-                                        <td class="whitespace-nowrap  px-6 py-4 ">Jacob</td>
-                                        <td class="whitespace-nowrap  px-6 py-4">Thornton</td>
-                                        <td class="whitespace-nowrap  px-6 py-4">@fat</td>
-                                    </tr>
-                                    <tr class="border-b dark:border-neutral-500">
-                                        <td class="whitespace-nowrap  px-6 py-4 font-medium">3</td>
-                                        <td colspan="2" class="whitespace-nowrap  px-6 py-4">
-                                            Larry the Bird
-                                        </td>
-                                        <td class="whitespace-nowrap  px-6 py-4">@twitter</td>
-                                    </tr>
+
                                 </tbody>
                             </table>
                         </div>
