11a426f8ace3a116ea5292183452c5ad519f13a3
Glicht <rosalesvallorani@gmail.com>
2023-07-10 08:33:34 -0400

    CAmbios

--- admin/modulos/Usuarios/index.php
+++ admin/modulos/Usuarios/index.php
@@ -88,8 +88,8 @@
                 </div>
 
                 <!--Modal body-->
-                <div class="relative p-4">
-                    <div class="relative w-full flex flex-col gap-3" data-te-input-wrapper-init>
+                <div class="relative p-4 flex flex-col gap-3">
+                    <div class="relative w-full " data-te-input-wrapper-init>
                         <input modal_editar="input" maxlength="40" type="text" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="modal_editar_usuario_nombre" id="modal_editar_usuario_nombre" placeholder="Example label" />
                         <label for="modal_editar_usuario_nombre" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200">Nombre
                         </label>
