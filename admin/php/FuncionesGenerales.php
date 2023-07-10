51cb936119c9dc7ef37e72da947f58ee08cdb82d
Glicht <rosalesvallorani@gmail.com>
2023-07-10 11:00:22 -0400

    Cambios

--- admin/php/FuncionesGenerales.php
+++ admin/php/FuncionesGenerales.php
@@ -310,13 +310,12 @@ function validar_string($string, $permitidos)
 
 function validar_string2($string, $permitidos) //incluye que no permita solo caracteres en blanco
 {
+    $string = trim($string);
+    if (strlen($string) == 0) {
+        return false; // La cadena solo contiene espacios en blanco
+    }
     for ($i = 0; $i < strlen($string); $i++) {
-        if (
-            empty($string[$i]) || // No permitir caracteres vacíos o en blanco
-            strpos($permitidos, $string[$i]) === false &&
-            $string[$i] !== " " && // Agrega un espacio en blanco
-            $string[$i] !== "\n"
-        ) { // Agrega un salto de línea
+        if (strpos($permitidos, $string[$i]) === false && $string[$i] !== "\n") {
             return false;
         }
     }
