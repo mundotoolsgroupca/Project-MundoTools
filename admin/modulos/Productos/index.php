50474a65b1fdf7651769cb1cbe00fce1f67c1396
Glicht <rosalesvallorani@gmail.com>
2023-07-24 13:49:35 -0400

    Cambios

--- admin/modulos/Productos/index.php
+++ admin/modulos/Productos/index.php
@@ -1,10 +1,6 @@
 <script>
     $('#principal').off();
 </script>
-<!---------Select2-------->
-<link rel="stylesheet" href="./assets/css/select2.css" />
-<script src="./assets/js/select2.js"></script>
-
 <div class="[ h-full flex gap-3 flex-col ] [ lg:flex-row ]">
     <!-- Modulo -->
     <div class="[ h-auto  rounded-lg  bg-white p-6 shadow-lg w-full ] [   md:overflow-y-auto ] [ lg:w-1/5   lg:overflow-y-auto ]">
@@ -172,7 +168,7 @@
                                 </select>
                             </div>
                             <div class="relative mb-3 w-full" data-te-input-wrapper-init>
-                                <textarea maxlength="500" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="ModalEditarDescripcion" id="ModalEditarDescripcion" rows="4" placeholder="Descripcion"></textarea>
+                                <textarea maxlength="300" class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" name="ModalEditarDescripcion" id="ModalEditarDescripcion" rows="4" placeholder="Descripcion"></textarea>
                                 <label for="ModalEditarDescripcion" class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200">Descripcion
                                 </label>
                             </div>
@@ -450,8 +446,6 @@
 
 <script id="tailwindelements_script" src="./assets/js/tw-elements.umd.min.js"></script>
 <script>
-    $('#categoria').select2();
-
     if (typeof tabla_grupos_producto === 'undefined') {
         let tabla_grupos_producto = new DataTable('#tabla_grupos_producto');
     }
