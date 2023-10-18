4aa145112b5331d9bebaba51d0b64b989b059419
tipoxd <rosalesvallorani@gmail.com>
2023-10-18 16:27:42 -0400

    lorem

--- search/index.php
+++ search/index.php
@@ -1173,12 +1173,12 @@ if (isset($_SESSION['token'])) {
                 data.map((item) => {
                     $('#navcategoria').append(`
         <li>
-            <a href='../search/?categoria=${item.id}' class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" href="#" data-te-dropdown-item-ref>${item.nombre}</a>
+            <a href='../search/?categoria=${item.id}' class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200  " href="#" data-te-dropdown-item-ref>${item.nombre}</a>
         </li>`);
 
                     $('#navcategoria_mobile').append(`
         <li>
-            <a href='../search/?categoria=${item.id}' class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600" href="#" data-te-dropdown-item-ref>${item.nombre}</a>
+            <a href='../search/?categoria=${item.id}' class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200  " href="#" data-te-dropdown-item-ref>${item.nombre}</a>
         </li> `);
                 });
 
